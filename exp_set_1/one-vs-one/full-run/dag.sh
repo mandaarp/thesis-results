0f420e60a87ee95fa587782b12d0e7a39eea58c2
Mandar Patil <mandaarp@gmail.com>
2013-01-17 22:45:30 -0800

    dag.sh evaluation

---
+++ exp_set_1/one-vs-one/full-run/dag.sh
@@ -0,0 +1,34 @@
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test2"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+echo "test1"
+python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+
+
