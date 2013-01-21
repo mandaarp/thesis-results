481bb86c71a94b5881445f69fa37fafa113efd65
Mandar Patil <mandaarp@gmail.com>
2013-01-20 21:16:30 -0800

    rbf execution

--- exp_set_1/one-vs-one/full-run/dag.sh
+++ exp_set_1/one-vs-one/full-run/dag.sh
@@ -1,34 +1,74 @@
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+source ~/.bashrc
 
-echo "test2"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+EXE="/u/mandar/thesis-code/experimental-code/DAG.py"
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test1"
+SYSTEM=${SYSTEMS[0]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test2"
+SYSTEM=${SYSTEMS[1]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test3"
+SYSTEM=${SYSTEMS[2]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test4"
+SYSTEM=${SYSTEMS[3]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test5"
+SYSTEM=${SYSTEMS[4]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test6"
+SYSTEM=${SYSTEMS[5]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test7"
+SYSTEM=${SYSTEMS[6]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test8"
+SYSTEM=${SYSTEMS[7]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
 
-echo "test1"
-python ~/thesis-code/experimental-code/DAG.py -f "01-13-2013-macaroni/test1/decision_values.csv"
+TEST="test9"
+SYSTEM=${SYSTEMS[8]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
+
+TEST="test10"
+SYSTEM=${SYSTEMS[9]}
+echo "$TEST on $SYSTEM"
+FILE="/u/mandar/thesis-results/exp_set_1/one-vs-one/full-run/01-13-2013-$SYSTEM/$TEST/decision_values.csv"
+python ~/thesis-code/experimental-code/DAG.py -f $FILE
+
+
+
+
+
+
+
+
+
 
 
