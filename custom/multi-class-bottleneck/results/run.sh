cc9c06d859dd2a4ce0793c22663deccf6bda1b2a
Mandar Patil <mandaarp@gmail.com>
2013-02-12 22:14:32 -0800

    multiclass.py results

---
+++ custom/multi-class-bottleneck/results/run.sh
@@ -0,0 +1,40 @@
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-50_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-100_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-150_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-200_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-250_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-300_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-350_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-400_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-450_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
+FILE=${HOME}/thesis-results/custom/multi-class-bottleneck/data/decision_values_one-vs-one_N-500_Imprint.csv
+echo "File: ${FILE}"
+python ~/thesis-code/experimental_code/custom/multiclass.py -f ${FILE}
+
